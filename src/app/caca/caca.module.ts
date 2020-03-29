8a32c240635f0143112a971c2005b6ef05964e3f
luisferlie <luisferlie@gmail.com>
2020-03-29 11:01:06 +0200

    first commit

---
+++ src/app/caca/caca.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CacaRoutingModule } from './caca-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CacaRoutingModule
+  ]
+})
+export class CacaModule { }
